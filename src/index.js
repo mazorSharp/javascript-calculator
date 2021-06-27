af40d2ddb53bddd1fede0e2c36c7b06bfb9cb9f8
mazorSharp <85122280+mazorSharp@users.noreply.github.com>
2021-06-27 11:10:48 -0600

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,160 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const ops = [ '/', '*', '-', '+'];
+const ids = {
+  7: 'seven', 
+  8: 'eight', 
+  9: 'nine', 
+  4: 'four', 
+  5: 'five', 
+  6: 'six', 
+  1: 'one', 
+  2: 'two', 
+  3: 'three', 
+  0: 'zero',
+  '/': 'divide', 
+  '*': 'multiply', 
+  '-': 'subtract', 
+  '+': 'add'
+}
+
+class App extends React.Component {
+  state = {
+    lastPressed: undefined,
+    calc: '0',
+    operation: undefined
+  }
+  
+  
+  handleClick = (e) => {
+    const { calc, lastPressed } = this.state;
+    const { innerText } = e.target;
+    
+    switch(innerText) {
+      case 'AC': {
+        this.setState({
+          calc: '0',
+        });
+        break;
+      }
+        
+      case '=': {
+        const evaluated = eval(calc);
+        this.setState({
+          calc: evaluated
+        });
+        break;
+      }
+        
+      case '.': {
+        const splitted = calc.split(/[\+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+        
+        if(!last.includes('.')) {
+          this.setState({
+            calc: calc+'.'
+          })
+        }
+        
+        break;
+      }
+        
+      default: {
+        let e = undefined;
+        // check for other op
+        if(ops.includes(innerText)) {
+          if(ops.includes(lastPressed) && innerText !== '-') {
+            // oh boii...
+            const lastNumberIdx = calc.split('').reverse()
+                .findIndex(char => char !== ' ' && nums.includes(+char)); 
+            e = calc.slice(0, calc.length - lastNumberIdx) + ` ${innerText} `;
+          } else {
+            e = `${calc} ${innerText} `;
+          }
+        } else {
+          e = (calc === '0') ? innerText : (calc + innerText);
+        }
+        
+        this.setState({
+          calc: e
+        });
+      }
+    }
+    
+    this.setState({
+      lastPressed: innerText
+    })
+    
+  }
+  
+  render() {
+    const { currentNumber, calc } = this.state;
+    
+    return (
+      <div className="calculator">        
+        <div id="display" className="display">
+          {calc}
+        </div>
+        
+        <div className="nums-container">
+          <button 
+            className="big-h light-grey ac" 
+            onClick={this.handleClick} 
+            id="clear"
+            >
+            AC
+          </button>
+          
+          {nums.map(num => (
+            <button 
+              className={`dark-grey ${num === 0 && 'big-h'}`} 
+              key={num} 
+              onClick={this.handleClick}
+              id={ids[num]}
+             >
+              {num}
+            </button>
+          ))}
+          
+          <button 
+            className="light-grey" 
+            onClick={this.handleClick} 
+            id="decimal"
+           >
+            .
+          </button>
+        </div>
+        <div className="ops-container">
+          {ops.map(op => (
+            <button 
+              className="orange" 
+              key={op} 
+              onClick={this.handleClick}
+              id={ids[op]}
+             >
+              {op}
+            </button>
+          ))}
+          
+          <button 
+            className="orange" 
+            onClick={this.handleClick} 
+            id="equals"
+           >
+            =
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+     <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
